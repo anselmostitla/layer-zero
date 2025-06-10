31c4f4f3f9a6736d23cec867d95e733ee1ec981b
anselmo <anselmostitla@gmail.com>
2025-06-10 16:16:44 -0600

    Changing OFTV2 with OFT

--- src/MyOFT.sol
+++ src/MyOFT.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.20;
 
-import {OFTV2} from "lib/solidity-examples/contracts/token/oft/v2/OFTV2.sol";
+import {OFT} from "lib/solidity-examples/contracts/token/oft/v1/OFT.sol";
 
-contract MyOFT is OFTV2 {
+contract MyOFT is OFT {
    constructor(
          string memory _name,
         string memory _symbol,
-        uint8 _sharedDecimals,
+      //   uint8 _sharedDecimals,
         address _lzEndpoint
-   ) OFTV2(_name, _symbol, _sharedDecimals, _lzEndpoint) {}
+   ) OFT(_name, _symbol, _lzEndpoint) {}
 
    function mint(address to, uint256 amount) external {
       _mint(to, amount);
